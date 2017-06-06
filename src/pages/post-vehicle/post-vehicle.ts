442bcf38434f5dabd0890c2a3dcffbde1c223e93
adriano515 <valdezadriano@gmail.com>
2017-06-06 03:00:03 -0600

    changes to upload files

--- src/pages/post-vehicle/post-vehicle.ts
+++ src/pages/post-vehicle/post-vehicle.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 import { UserService } from '../../providers/user-service';
 import { NavController, NavParams, ViewController, AlertController, LoadingController } from 'ionic-angular';
+import { FileChooser } from '@ionic-native/file-chooser';
+
+import firebase from 'firebase';
 
 @Component({
   selector: 'page-post-vehicle',
   templateUrl: 'post-vehicle.html',
-  providers: [UserService],
+  providers: [UserService, FileChooser],
 })
 export class PostVehicle {
 
@@ -26,10 +29,15 @@ export class PostVehicle {
   public priceField: any;
   public descField: any;
   public phoneField: any;
+  public image: any;
+
+  nativepath: any;
+  firestore = firebase.storage();
+
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private viewCtrl: ViewController,
     public userService: UserService, public alertCtrl: AlertController,
-    public loadingCtrl: LoadingController) {
+    public loadingCtrl: LoadingController,public zone: NgZone, public fileChooser: FileChooser) {
   }
 
   // This is a helper function to display messages.
@@ -60,6 +68,7 @@ export class PostVehicle {
     this.priceField = undefined;
     this.descField = undefined;
     this.phoneField = undefined;
+    this.image = undefined;
   }
 
   removeWhitespaces() {
@@ -94,7 +103,7 @@ export class PostVehicle {
     if (this.brandField == undefined || this.modelField == undefined ||
     this.yearField == undefined || this.priceField == undefined ||
     this.phoneField == undefined || this.descField == undefined ||
-    this.currencyField == undefined) {
+    this.currencyField == undefined || this.image == undefined) {
       // If any of these fields are blank then you can't publish the vehicle.
       this.showAlert("Error!", "Por favor llenar minimo los campos de marca, modelo, año, precio, descripción y numero de contacto.");
     } else {
@@ -144,11 +153,13 @@ export class PostVehicle {
         this.odomField = Math.floor(this.odomField * 100) / 100;
       };
 
+      /*
       // Generate a random number for the image. This is a placeholder.
       var random = Math.floor(Math.random() * 20);
       if (random < 1) { random += 1;};
       if (random > 20) {random = 20;};
       var image = random + ".jpg";
+      */
 
       // Now post the car.
       this.userService.postVehicle(this.brandField, this.modelField,
@@ -156,7 +167,7 @@ export class PostVehicle {
         this.typeField, this.cylindersField, this.engineSizeField,
         this.transField, this.extColorField, this.intColorField,
         this.intTypeField, this.originField, this.currencyField,
-        this.priceField, this.descField, this.phoneField, image).then(() => {
+        this.priceField, this.descField, this.phoneField, this.image).then(() => {
           this.clearView();
           this.showAlert("Gracias!", "El vehiculo fue publicado con exito!");
         });
@@ -164,4 +175,42 @@ export class PostVehicle {
     };
   }
 
+  store(){
+
+    this.fileChooser.open().then((url) => {
+      (<any>window).FilePath.resolveNativePath(url, (result) => {
+          this.nativepath = result;
+          this.uploadimage();
+        }
+      )
+    })
+  }
+
+  uploadimage() {
+    (<any>window).resolveLocalFileSystemURL(this.nativepath, (res) => {
+      res.file((resFile) => {
+        var reader = new FileReader();
+        reader.readAsArrayBuffer(resFile);
+        reader.onloadend = (evt: any) => {
+          var imgBlob = new Blob([evt.target.result], { type: 'image/jpeg' });
+          var imageStore = this.firestore.ref().child('image');
+          imageStore.put(imgBlob).then((res) => {
+            alert('Upload Success');
+          }).catch((err) => {
+            alert('Upload Failed' + err);
+          })
+        }
+      })
+    })
+  }
+
+  display() {
+    this.firestore.ref().child('image').getDownloadURL().then((url) => {
+      this.zone.run(() => {
+        this.image = url;
+      })
+    })
+  }
+
 }
+
